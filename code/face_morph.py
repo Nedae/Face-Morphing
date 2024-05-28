232a297e85ee806459d6ee0e79c2cf92291e4b75
nedae <neda.eskandari@gmail.com>
2024-05-28 23:21:31 +0000

    fix saving gif

--- code/face_morph.py
+++ code/face_morph.py
@@ -56,52 +56,70 @@ def morph_triangle(img1, img2, img, t1, t2, t, alpha) :
     img[r[1]:r[1]+r[3], r[0]:r[0]+r[2]] = img[r[1]:r[1]+r[3], r[0]:r[0]+r[2]] * ( 1 - mask ) + imgRect * mask
 
 
-def generate_morph_sequence(duration,frame_rate,img1,img2,points1,points2,tri_list,size,output):
-
-    num_images = int(duration*frame_rate)
-    p = Popen(['ffmpeg', '-y', '-f', 'image2pipe', '-r', str(frame_rate),'-s',str(size[1])+'x'+str(size[0]), '-i', '-', '-c:v', 'libx264', '-crf', '25','-vf','scale=trunc(iw/2)*2:trunc(ih/2)*2','-pix_fmt','yuv420p', output], stdin=PIPE)
+def generate_morph_sequence(duration, frame_rate, img1, img2, points1, points2, tri_list, size, output):
+    num_images = int(duration * frame_rate)
     
-    for j in range(0, num_images):
-
-        # Convert Mat to float data type
+    # Generate palette
+    palette_proc = Popen([
+        'ffmpeg', '-y', '-f', 'image2pipe', '-r', str(frame_rate), '-s', f'{size[1]}x{size[0]}', '-i', '-',
+        '-vf', 'palettegen', '-t', str(duration), '-y', 'palette.png'
+    ], stdin=PIPE)
+    
+    for j in range(num_images):
         img1 = np.float32(img1)
         img2 = np.float32(img2)
-
-        # Read array of corresponding points
         points = []
-        alpha = j/(num_images-1)
-
-        # Compute weighted average point coordinates
-        for i in range(0, len(points1)):
+        alpha = j / (num_images - 1)
+        for i in range(len(points1)):
             x = (1 - alpha) * points1[i][0] + alpha * points2[i][0]
             y = (1 - alpha) * points1[i][1] + alpha * points2[i][1]
-            points.append((x,y))
-        
-        # Allocate space for final output
-        morphed_frame = np.zeros(img1.shape, dtype = img1.dtype)
+            points.append((x, y))
 
-        for i in range(len(tri_list)):    
-            x = int(tri_list[i][0])
-            y = int(tri_list[i][1])
-            z = int(tri_list[i][2])
-            
-            t1 = [points1[x], points1[y], points1[z]]
-            t2 = [points2[x], points2[y], points2[z]]
-            t = [points[x], points[y], points[z]]
-
-            # Morph one triangle at a time.
+        morphed_frame = np.zeros(img1.shape, dtype=img1.dtype)
+        for i in range(len(tri_list)):
+            x, y, z = int(tri_list[i][0]), int(tri_list[i][1]), int(tri_list[i][2])
+            t1, t2, t = [points1[x], points1[y], points1[z]], [points2[x], points2[y], points2[z]], [points[x], points[y], points[z]]
             morph_triangle(img1, img2, morphed_frame, t1, t2, t, alpha)
-            
-            pt1 = (int(t[0][0]), int(t[0][1]))
-            pt2 = (int(t[1][0]), int(t[1][1]))
-            pt3 = (int(t[2][0]), int(t[2][1]))
-
+            pt1, pt2, pt3 = (int(t[0][0]), int(t[0][1])), (int(t[1][0]), int(t[1][1])), (int(t[2][0]), int(t[2][1]))
             cv2.line(morphed_frame, pt1, pt2, (255, 255, 255), 1, 8, 0)
             cv2.line(morphed_frame, pt2, pt3, (255, 255, 255), 1, 8, 0)
             cv2.line(morphed_frame, pt3, pt1, (255, 255, 255), 1, 8, 0)
-            
+        
         res = Image.fromarray(cv2.cvtColor(np.uint8(morphed_frame), cv2.COLOR_BGR2RGB))
-        res.save(p.stdin,'JPEG')
+        res.save(palette_proc.stdin, 'JPEG')
 
-    p.stdin.close()
-    p.wait()
+    palette_proc.stdin.close()
+    palette_proc.wait()
+
+    # Generate GIF with palette
+    gif_proc = Popen([
+        'ffmpeg', '-y', '-f', 'image2pipe', '-r', str(frame_rate), '-s', f'{size[1]}x{size[0]}', '-i', '-',
+        '-i', 'palette.png', '-lavfi', 'fps=10,scale=320:-1:flags=lanczos[x];[x][1:v]paletteuse',
+        '-loop', '0', '-gifflags', '+transdiff', '-y', output
+    ], stdin=PIPE)
+
+    for j in range(num_images):
+        img1 = np.float32(img1)
+        img2 = np.float32(img2)
+        points = []
+        alpha = j / (num_images - 1)
+        for i in range(len(points1)):
+            x = (1 - alpha) * points1[i][0] + alpha * points2[i][0]
+            y = (1 - alpha) * points1[i][1] + alpha * points2[i][1]
+            points.append((x, y))
+
+        morphed_frame = np.zeros(img1.shape, dtype=img1.dtype)
+        for i in range(len(tri_list)):
+            x, y, z = int(tri_list[i][0]), int(tri_list[i][1]), int(tri_list[i][2])
+            t1, t2, t = [points1[x], points1[y], points1[z]], [points2[x], points2[y], points2[z]], [points[x], points[y], points[z]]
+            morph_triangle(img1, img2, morphed_frame, t1, t2, t, alpha)
+            pt1, pt2, pt3 = (int(t[0][0]), int(t[0][1])), (int(t[1][0]), int(t[1][1])), (int(t[2][0]), int(t[2][1]))
+            cv2.line(morphed_frame, pt1, pt2, (255, 255, 255), 1, 8, 0)
+            cv2.line(morphed_frame, pt2, pt3, (255, 255, 255), 1, 8, 0)
+            cv2.line(morphed_frame, pt3, pt1, (255, 255, 255), 1, 8, 0)
+        
+        res = Image.fromarray(cv2.cvtColor(np.uint8(morphed_frame), cv2.COLOR_BGR2RGB))
+        res.save(gif_proc.stdin, 'JPEG')
+
+    gif_proc.stdin.close()
+    gif_proc.wait()
