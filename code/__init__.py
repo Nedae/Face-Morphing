0cdc247285a26bdf1e4bdaeb45e91dce23aa1a51
nedae <neda.eskandari@gmail.com>
2024-05-29 23:15:12 +0000

    initial local morph -- bbox

--- code/__init__.py
+++ code/__init__.py
@@ -1,6 +1,6 @@
 from face_landmark_detection import generate_face_correspondences
 from delaunay_triangulation import make_delaunay
-from face_morph import generate_morph_sequence, generate_weighted_image
+from face_morph import generate_morph_sequence, generate_weighted_image, local_morph
 
 import subprocess
 import argparse
@@ -19,7 +19,11 @@ def doMorphingImage(img1, img2, alpha, output):
     tri = make_delaunay(size[1], size[0], list3, img1, img2)
     generate_weighted_image(img1, img2, points1, points2, tri, size, alpha, output)
     
-
+def doLocalMorphing(img1, img2, alpha, output):
+    [size, img1, img2, points1, points2, list3] = generate_face_correspondences(img1, img2)
+    tri = make_delaunay(size[1], size[0], list3, img1, img2)
+    local_morph(img1, img2, points1, points2, tri, alpha, output)
+    
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
@@ -29,6 +33,7 @@ if __name__ == "__main__":
     parser.add_argument("--frame", type=int, default=20, help="The frameame Rate")
     parser.add_argument("--output", required=True, help="Output file path (GIF or PNG/JPG)")
     parser.add_argument("--alpha", default=0.5, type=float, help="Alpha value for single image morphing (0 to 1)")
+    parser.add_argument("--local", action="store_true", help="Perform local morphing")
 
     args = parser.parse_args()
 
@@ -37,6 +42,10 @@ if __name__ == "__main__":
 
     # Determine if the output is a GIF or a single image
     if args.output.lower().endswith('.gif'):
+        assert not args.local, "Local morphing can only be done when the output is an image type (PNG/JPG)"
         doMorphing(image1, image2, args.duration, args.frame, args.output)
     else:
-        doMorphingImage(image1, image2, args.alpha, args.output)
+        if args.local:
+            doLocalMorphing(image1, image2, args.alpha, args.output)
+        else:
+            doMorphingImage(image1, image2, args.alpha, args.output)
